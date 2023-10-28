5bf903bc1c39445e98ee6def81763fc00fdcf632
Jonathan Wofford <jonathanbrodie91@gmail.com>
2023-10-28 13:29:17 +0200

    added user auth

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -44,7 +44,7 @@ export const login = (email, password) => async (dispatch) => {
 	if (response.ok) {
 		const data = await response.json();
 		dispatch(setUser(data));
-		return null;
+		return data;
 	} else if (response.status < 500) {
 		const data = await response.json();
 		if (data.errors) {
