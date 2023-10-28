5c97fa25971c1a4291fed002415dd9ab77937c21
flalom <flavio.lombardo@unibas.ch>
2023-10-28 22:50:07 +0200

    WIP fetching data function and one hot encoding of discrete

--- backend/src/db/dbFetcher.py
+++ backend/src/db/dbFetcher.py
@@ -1,24 +1,38 @@
 from fastapi import FastAPI, HTTPException
 from supabase import create_client, Client
+from dotenv import load_dotenv   
+import numpy as np
+import pandas as pd
+import json
+import os 
 
+# Load environment variables from .env file
+load_dotenv()                    
 
-# Fetch data from the `users` table that correspond to a specific time range
-async def fetch_users_by_time_range(start_time, end_time):
+# Get the credentials from the environment variables
+SUPABASE_URL = os.environ.get('SUPABASE_URL')
+SUPABASE_ANON_KEY = os.environ.get('SUPABASE_ANON_KEY')
 
-  app = FastAPI()
+# Create a Supabase client
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
 
-  # Replace these values with your Supabase project credentials
-  SUPABASE_URL = "https://your-supabase-url.supabase.co"
-  SUPABASE_ANON_KEY = "your-supabase-anon-key"
+# Create a FastAPI app
+app = FastAPI()
 
-  supabase: Client = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
+@app.get("/encoded_data/{time_slot}/{location}")
+async def get_encoded_data(time_slot: int, location: str):
+    # Fetch data
+    response = await supabase.table("items").select().eq("time_slot", time_slot).eq("location", location).execute()
+    data = response.get("data")
+    
+    if not data:
+        raise HTTPException(status_code=404, detail="Data not there")
+    
+    # Transform data
+    df = pd.DataFrame(data)
+    
+    # One-hot encode data
+    one_hot_encoded_df = pd.get_dummies(df, columns=['WIP'])  # wip - replace with actual column names
+    #one_hot_encoded_df.to_dict(orient='records') 
 
-  @app.get("/items/{item_id}")
-  async def read_item(item_id: int):
-      response = await supabase.table("items").select().eq("id", item_id).execute()
-      item = response.get("data")
-      if item:
-          return item
-      raise HTTPException(status_code=404, detail="Item not found")
-  
-  
+    return one_hot_encoded_df
