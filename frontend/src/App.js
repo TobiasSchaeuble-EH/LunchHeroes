2822f21490d6e5d4029882432881d07c021a8ea3
Jonathan Wofford <jonathanbrodie91@gmail.com>
2023-10-28 13:36:23 +0200

    added turinary to App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Login /> {/* Add the Login component here */}
+      <Routes>
+        {user ? (
+          //routes that will be available ONLY when user is logged in
+          //add additional routes here
+          <Route element={<Home />} path="/home" />
+        ) : (
+          //will redirect to '/' from any url if no user is logged in
+          //do not add additional routes here
+          <Route path="*" element={<Navigate to="/" replace />} />
+        )}
+        //login form will always be available //any route added here will always
+        be available
+        <Route element={<Login />} path="/" />
+      </Routes>
       </header>
     </div>
   );
