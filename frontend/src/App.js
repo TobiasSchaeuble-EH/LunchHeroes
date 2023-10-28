cb981e0f672ce65428dc47f86da9f2ed377a1e75
tobiasschaeuble-eh <tobais.schaeuble@endress.com>
2023-10-28 15:10:24 +0200

    wip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,48 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Login from './Components/Login';  // import the Login component
+import Login from './Components/Login'; 
+import Profile from './Components/Profile'; 
 
 function App() {
+  const userDetails = {
+    name: "John Doe",
+    email: "johndoe@example.com",
+    location: "New York, USA",
+    department: "Engineering",
+    groupSize: 3,
+    profilePicture: "./profile.png",
+    timeSlot: "10:00 AM - 11:00 AM",
+    interests: ["Coding", "Traveling", "Reading"]
+  };
+
+  const [groupSize, setGroupSize] = useState(userDetails.groupSize);
+  const [timeSlot, setTimeSlot] = useState(userDetails.timeSlot);
+
+  const handleGroupSizeChange = (size) => {
+    setGroupSize(size);
+  }
+
+  const handleTimeSlotChange = (slot) => {
+    setTimeSlot(slot);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <Login /> {/* Add the Login component here */}
+        <Login />
+        <Profile 
+          name={userDetails.name} 
+          email={userDetails.email}
+          location={userDetails.location}
+          department={userDetails.department}
+          groupSize={groupSize}
+          timeSlot={timeSlot}
+          profilePicture={userDetails.profilePicture}
+          interests={userDetails.interests}
+          onGroupSizeChange={handleGroupSizeChange}
+          onTimeSlotChange={handleTimeSlotChange}
+        />
       </header>
     </div>
   );
