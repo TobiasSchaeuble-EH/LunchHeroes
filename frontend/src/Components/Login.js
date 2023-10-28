a905540b43dee11faf011d218261558da02a4734
tobiasschaeuble-eh <tobais.schaeuble@endress.com>
2023-10-28 14:15:00 +0200

    init login

---
+++ frontend/src/Components/Login.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Here you can send the username and password to your server
+    console.log('Username:', username, 'Password:', password);
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            Username:
+            <input 
+              type="text" 
+              value={username} 
+              onChange={(e) => setUsername(e.target.value)} 
+              required 
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Password:
+            <input 
+              type="password" 
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)} 
+              required 
+            />
+          </label>
+        </div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
