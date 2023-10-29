6c4f3685625f228c17c8b932969b03229aac2c2e
Philipp <philnue@gmail.com>
2023-10-29 11:54:55 +0100

    Fixed some python data

--- backend/src/routes/users/userFunctions.py
+++ backend/src/routes/users/userFunctions.py
@@ -12,5 +12,7 @@ def getAllUsers():
     
 
 def getAllQueryListData():
-    return  supabase_client.table("waiting_query").select(f"*, user({users_select}), time_range(*), company(*), age_range(*)").execute()
+    #return  supabase_client.table("waiting_query").select(f"*, user({users_select}), time_range(*), company(*), age_range(*)").execute()
+    data, count =  supabase_client.table("waiting_query").select("id:user").execute()
+    return data
     
