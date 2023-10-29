ab87b17c4af54e40a5c72d45459d5e6d7956981a
Philipp Nüßlein <65328894+Philnue@users.noreply.github.com>
2023-10-29 10:03:28 +0100

    Updated Api for button trigger

--- backend/src/routes/users/userFunctions.py
+++ backend/src/routes/users/userFunctions.py
@@ -1,12 +1,11 @@
 from supabase_client import supabase_client
 
 
-users_select = "*, company:companies(*)"
+users_select = "*, company:companies(*), users_interests(*, interest(*, category(*))), age_range(*)"
 
 def getUserWithId(userId):
     return supabase_client.table("users").select(users_select).eq("id", userId).single().execute()
 
-from supabase_client import supabase_client
 
 def getAllUsers():
     return  supabase_client.table("users").select(users_select).execute()
